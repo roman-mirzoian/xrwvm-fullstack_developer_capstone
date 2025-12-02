6ce83004b01dcfd48af69a86ed2562373ca9d960
Roman Mirzoian <mirzo1930@gmail.com>
2025-12-02 14:28:03 +0400

    fix: pylint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -45,7 +45,7 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+    #context = {}
 
     # Load JSON data from the request body
     data = json.loads(request.body)
@@ -55,12 +55,13 @@ def registration(request):
     last_name = data["lastName"]
     email = data["email"]
     username_exist = False
-    email_exist = False
+    #email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception as e:
+        print(e)
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
@@ -122,12 +123,14 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if request.user.is_anonymous == False:
+    if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
             response = post_review(data)
+            print(response)
             return JsonResponse({"status": 200})
-        except:
+        except Exception as e:
+            print(e)
             return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
